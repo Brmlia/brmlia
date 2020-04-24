4c7b4019d7958530991a65bd7f9e61093db6f517
Heidi Li <hel779@g.harvard.edu>
2020-04-24 14:33:05 -0500

    Updates

---
+++ precache-manifest.0c89934bca686d5caa76044c3b85072b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41d75ededba4a1aaf728571e249d5c7f",
+    "url": "/brmlia/index.html"
+  },
+  {
+    "revision": "1cf94544ba5d604e724c",
+    "url": "/brmlia/static/css/2.a5b4ddd2.chunk.css"
+  },
+  {
+    "revision": "c59b95dfb18830eb37e8",
+    "url": "/brmlia/static/css/main.b624c722.chunk.css"
+  },
+  {
+    "revision": "1cf94544ba5d604e724c",
+    "url": "/brmlia/static/js/2.5236e461.chunk.js"
+  },
+  {
+    "revision": "5e264a85301e6e685e86fe9eeca0f14e",
+    "url": "/brmlia/static/js/2.5236e461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c59b95dfb18830eb37e8",
+    "url": "/brmlia/static/js/main.a5015433.chunk.js"
+  },
+  {
+    "revision": "0a99d05ca8156e528a9e",
+    "url": "/brmlia/static/js/runtime-main.ac2a4795.js"
+  }
+]);
