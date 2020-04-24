cb87f87a6e5e0613bf1ea703925fba3df703b35b
evanimp <evan.imperato@gmail.com>
2020-04-24 18:54:45 -0400

    start with brmlia

---
+++ controller.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const url = require('url');
+
+module.exports = http.createServer((req, res) => {
+
+    var service = require('./service.js');
+    const reqUrl = url.parse(req.url, true);
+
+    // GET Annotations Endpoint
+    if (reqUrl.pathname == '/annot' && req.method === 'GET') {
+        console.log('Request Annotations:' +
+            req.method + ' Endpoint: ' +
+            reqUrl.pathname);
+
+        service.annotRequest(req, res);
+    }
+    // GET Image Endpoint
+    else if (reqUrl.pathname == '/image' && req.method === 'GET') {
+        console.log('Request Image:' +
+            req.method + ' Endpoint: ' +
+            reqUrl.pathname);
+
+        service.imageRequest(req, res);
+    }
+    // Invalid Endpoint
+    else {
+        console.log('Request Type:' +
+            req.method + ' Invalid Endpoint: ' +
+            reqUrl.pathname);
+
+        service.invalidRequest(req, res);
+
+    }
+});
---
+++ service.js
@@ -0,0 +1,54 @@
+const url = require('url');
+
+exports.annotRequest = function (req, res) {
+    const reqUrl = url.parse(req.url, true);
+
+    // Get the download directory
+    const downloadsFolder = require('downloads-folder');
+    console.log(downloadsFolder());
+    var f = "/annotations.txt";
+    var path = downloadsFolder() + f;
+
+    // Requiring fs module in which
+    // readFile function is defined.
+    const fs = require('fs')
+
+    fs.readFile(path, 'utf-8', (err, data) => {
+        if (err) throw err;
+
+        console.log(data);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(data));
+
+    })
+};
+
+exports.imageRequest = function (req, res) {
+    const reqUrl = url.parse(req.url, true);
+
+    // Get the download directory
+    const downloadsFolder = require('downloads-folder');
+    console.log(downloadsFolder());
+    var f = "/st.tif";
+    var path = downloadsFolder() + f;
+
+    // Requiring fs module in which
+    // readFile function is defined.
+    const fs = require('fs')
+
+    fs.readFile(path, null, (err, data) => {
+        if (err) throw err;
+
+        // console.log(data);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'image/tiff');
+        res.end(data);
+    })
+};
+
+exports.invalidRequest = function (req, res) {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Invalid Request');
+};
