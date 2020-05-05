eead5e5e5a9b390f8b40edd94969786e58fd887e
Heidi Li <hel779@g.harvard.edu>
2020-05-05 13:55:33 -0500

    Updates

---
+++ precache-manifest.2c76f27f9c2b67759923bbea42f73c5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0035ba2382190c8b13643958259b227",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf278cdec7f8bd9141bd",
+    "url": "/static/css/2.c5dc106f.chunk.css"
+  },
+  {
+    "revision": "0428afe3377857208540",
+    "url": "/static/css/main.98c7d86a.chunk.css"
+  },
+  {
+    "revision": "cf278cdec7f8bd9141bd",
+    "url": "/static/js/2.26ddaabb.chunk.js"
+  },
+  {
+    "revision": "9acd959cacaf822bfab829893c488b68",
+    "url": "/static/js/2.26ddaabb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0428afe3377857208540",
+    "url": "/static/js/main.45f8b63e.chunk.js"
+  },
+  {
+    "revision": "77874719f6eea7815e99",
+    "url": "/static/js/runtime-main.04f9b415.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/brmlia/precache-manifest.88c27c72845a5ed48fb6e2a43aefb19a.js"
+  "/precache-manifest.2c76f27f9c2b67759923bbea42f73c5d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/brmlia/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
